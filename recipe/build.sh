6b71b08db81f56a00d6d6a2895b8680bf423d805
Isuru Fernando <isuruf@gmail.com>
2017-05-25 11:32:55 +0530

    try gcc on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-
-# FIXME: This is a hack to make sure the environment is activated.
-# The reason this is required is due to the conda-build issue
-# mentioned below.
-#
-# https://github.com/conda/conda-build/issues/910
-#
-source activate "${CONDA_DEFAULT_ENV}"
+export CC=gcc
+export CXX=g++
 
 if [[ "$(uname)" == 'Darwin' ]];
 then
